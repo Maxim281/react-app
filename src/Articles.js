435d5268c186a28eab4008d63a1f316169a34319
Maksim Strelkov <maksim.strelkov29@mail.ru>
2020-12-07 22:55:06 +0400

    link

--- src/Articles.js
+++ src/Articles.js
@@ -5,6 +5,9 @@ class Articles extends Component {
       return (
          <div>
             <h2>Статьи</h2>
+            <p className="articles__link"><a href="#1">link 1</a></p>
+            <p className="articles__link"><a href="#2">link 2</a></p>
+            <p className="articles__link"><a href="#3">link 3</a></p>
          </div>
       );
    }
